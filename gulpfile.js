a0673fcb98dab87a7335e311b8474c6f119deebc
Jarret Gabel <jarretgabel@hotmail.com>
2015-10-06 12:34:53 -0600

    Added Velocity & Slick to dependencies via Bower

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var sass           = require('gulp-sass');
 var sourcemaps     = require('gulp-sourcemaps');
 var autoprefixer   = require('gulp-autoprefixer');
 var concat         = require('gulp-concat');
-var coffee         = require('gulp-coffee');
 var uglify         = require('gulp-uglify');
 var browserSync    = require('browser-sync');
 var reload         = browserSync.reload;
@@ -45,25 +44,11 @@ gulp.task('sass', function() {
     .pipe(reload({stream: true}));
 });
 
-gulp.task('coffee', function() {
-  gulp.src([
-    // This is like the Rails asset pipeline JS manifest
-    'assets/_coffee/global.coffee',
-  ])
-    .pipe(sourcemaps.init())
-    .pipe(concat('main.coffee'))
-    .pipe(coffee({bare: true}).on('error', function (err) {
-      console.log(err);
-      this.emit('end');
-    }))
-    .pipe(sourcemaps.write())
-    // .pipe(sourcemaps.write('./maps'))
-    .pipe(gulp.dest('assets/js/'));
-});
-
 gulp.task('uglify', function() {
   return gulp.src('assets/js/main.js')
+    .pipe(sourcemaps.init())
     .pipe(uglify())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('assets/js/'));
 });
 
@@ -91,8 +76,8 @@ gulp.task('browser-sync', function() {
 gulp.task('watch', function() {
   gulp.watch('_haml/**/*.haml', ['haml']);
   gulp.watch('assets/_sass/**/*.sass', ['sass']);
-  gulp.watch('assets/_coffee/**/*.coffee', ['coffee']);
-  // gulp.watch('assets/js/main.js', ['uglify']);
+  gulp.watch('assets/js/main.js', ['uglify']);
+  gulp.watch('bower_components/**', ['bower']);
 });
 
 // Default Task
@@ -100,7 +85,6 @@ gulp.task('default', [
   'bower',
   'haml',
   'sass',
-  'coffee',
   'uglify',
   'browser-sync',
   'watch'
